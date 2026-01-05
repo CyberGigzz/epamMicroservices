9ea9f568ace57c120ca13698e5d1e2db68d0ddd8
Giga <gigakatamadze0@gmail.com>
2026-01-05 13:22:24 +0400

    Made some changes. now every service works

--- api-gateway/src/main/java/com/gym/apigateway/filter/JwtAuthenticationFilter.java
+++ api-gateway/src/main/java/com/gym/apigateway/filter/JwtAuthenticationFilter.java
@@ -61,12 +61,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     private boolean isOpenEndpoint(String path, String method) {
-    if (path.equals("/api/auth/login")) {
-        return true;
+        if (path.equals("/api/auth/login")) {
+            return true;
+        }
+        if (path.equals("/api/trainees/register") || path.equals("/api/trainers/register")) {
+            return true;
+        }
+        return false;
     }
-    if (path.equals("/api/trainees/register") || path.equals("/api/trainers/register")) {
-        return true;
-    }
-    return false;
-}
 }
